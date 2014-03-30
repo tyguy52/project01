c55d1edac6223da5a431689b3bef5e272bfc623c
Tyler <tyguy52@users.noreply.github.com>
2014-03-30 18:51:25 -0400

    Update NumberRunner.java

--- src/project01/NumberRunner.java
+++ src/project01/NumberRunner.java
@@ -1,5 +1,3 @@
-package project01;
-
 import java.util.*;
 
 public class NumberRunner 
@@ -11,18 +9,21 @@ public class NumberRunner
 		int denom;
 		int[] rationalSplit = new int[2];
 		RationalNumber rn = new RationalNumber();
+		RationalNumber rn2 = new RationalNumber();
+
+		System.out.println("please enter two rational numbers");
+		String rational = input.nextLine();
+		String rational2 = input.nextLine();
 		
-		System.out.println("please enter a rational number");
-		String rational = input.next();
 		rationalSplit = rn.parseRational(rational);
 		numer = rationalSplit[0];
 		denom = rationalSplit[1];
+		rn = new RationalNumber(numer, denom);
 		
-		if(denom == 1)
-			rn = new RationalNumber(numer);
-		else
-			rn = new RationalNumber(numer, denom);
-			
+		rationalSplit = rn2.parseRational(rational2);
+		numer = rationalSplit[0];
+		denom = rationalSplit[1];
+		rn2 = new RationalNumber(numer,denom);
 
 		if (denom == 0)
 		{
@@ -38,7 +39,13 @@ public class NumberRunner
 		}	
 
 		System.out.println(rn);
-		System.out.println(RationalNumber.rationalAddition(13, 13.4));
+		System.out.println(RationalNumber.rationalAddition(rational, rational2));
+		System.out.println(rn.rationalAddition(rn2));
+		System.out.println(rn.rationalSubtraction(rn2));
+		System.out.println(rn.rationalMultiplication(rn2));
+		System.out.println(rn.rationalDivision(rn2));
+		System.out.println(rn.equals(rn2));
+		System.out.println(rn.compareTo(rn2));
 
 		input.close();
 	}
